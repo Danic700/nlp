16f8587e1fb5c9dffa870c66806e44a1f55e2351
zorik <zorik9@gmail.com>
2019-11-18 10:47:02 +0200

    add letters frequency & entropy

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords # Required for cleaning the text
+from scipy  import stats # Requried for calculating entropy
 
+
+def letterFrequency(txt):
+    from collections import Counter
+    from string import ascii_lowercase
+    c=Counter(txt.lower())
+    n=len(txt)/100.
+    ascii_lowercase += "."
+    print(ascii_lowercase)
+    return [(x, c[x]/n) for x in ascii_lowercase]
 
 # Loading the file
 file = open("DevilsDictionary.txt", "r")
@@ -9,8 +19,14 @@ book = file.read()
 stop_words = stopwords.words('english')
 clean_word_list = [word for word in book.split() if word not in stop_words]
 
+# Calculate a-z & '.' letters frequency
+lf = letterFrequency(' '.join(map(str,clean_word_list)))
+print(lf)
 
+# Get 2 lists: letters and their frequency
+qk, pk = zip(*lf)
 
+# Estimate the entropy of english letters based on the current corpus
+englishEntopyEstimate = stats.entropy(pk)
+print(englishEntopyEstimate)
 
-
-print(clean_word_list)
