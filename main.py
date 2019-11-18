7b6e4bb07f2cc1b4a407d9200c8c3b36d30000f8
Dani700 <reze.dani@gmail.com>
2019-11-18 20:33:17 +0200

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from collections import Counter
 
 
 # Loading the file
 file = open("DevilsDictionary.txt", "r")
-book = file.read()
+book = file.read().lower()
 
 # Cleaning the text
 stop_words = stopwords.words('english')
 clean_word_list = [word for word in book.split() if word not in stop_words]
 
+# Finding letter frequency
+letters = Counter(''.join(clean_word_list))
 
-
-
-
+print(letters)
 print(clean_word_list)
+
