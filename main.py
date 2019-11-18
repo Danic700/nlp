6ecf516d15acbcae649efbbfb0bcc81f4498fb17
Dani700 <reze.dani@gmail.com>
2019-11-18 22:00:55 +0200

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
-from nltk.probability import FreqDist
+import numpy as np
 from collections import Counter
-
 
 # Loading the file
 file = open("DevilsDictionary.txt", "r")
@@ -12,8 +11,23 @@ stop_words = stopwords.words('english')
 clean_word_list = [word for word in book.split() if word not in stop_words]
 
 # Finding letter frequency
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', '']
+
 letters = Counter(''.join(clean_word_list))
+filtered_items = [(k, v) for (k, v) in letters.items() if k in alphabet]
+filtered_items = dict(filtered_items)
+num_of_characters = len(clean_word_list.__str__())
+for k, v in filtered_items.items():
+    filtered_items[k] = v/num_of_characters
 
-print(letters)
+
+# Calculating entropy
+
+
+
+
+
+print(filtered_items)
 print(clean_word_list)
-
+print("Num of charcters", num_of_characters)
