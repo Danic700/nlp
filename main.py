4a5e27f9c22690e7f7570e8b2a18fb258b051e40
adva2sh <adva2sh@gmail.com>
2019-11-19 19:17:17 +0200

    a

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
 import numpy as np
 from collections import Counter
+import string
+import re
 
 # Loading the file
-file = open("DevilsDictionary.txt", "r")
-book = file.read().lower()
+with open("DevilsDictionary.txt", "r") as file:
+    book = file.read().lower()
 
-# Cleaning the text
-stop_words = stopwords.words('english')
-clean_word_list = [word for word in book.split() if word not in stop_words]
+    # Remove everything except a-z and space
+    book = re.sub('[^a-z ]+', '', book)
 
-# Finding letter frequency
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', '']
+    # Cleaning the text from stopwords (but keep the spaces)
+    stop_words = set(stopwords.words('english'))
+    for stopword in stop_words:
+        book = book.replace(' '+stopword+' ', ' ')
+        
+    # Finding letter frequency
+    alphabet = list(string.ascii_lowercase)+[' ']
 
-letters = Counter(''.join(clean_word_list))
-filtered_items = [(k, v) for (k, v) in letters.items() if k in alphabet]
-filtered_items = dict(filtered_items)
-num_of_characters = len(clean_word_list.__str__())
-for k, v in filtered_items.items():
-    filtered_items[k] = v/num_of_characters
+    letters_freq = Counter(book)
+
+    print(letters_freq)
+    
+    total_chars = len(book)
 
 
 # Calculating entropy
-
-
-
-
-
-print(filtered_items)
-print(clean_word_list)
-print("Num of charcters", num_of_characters)
